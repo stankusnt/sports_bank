dcd3fc3512c8ab8e296101a5f85e25401d9d62ba
Personal <stankusnt@gmail.com>
2024-09-27 07:53:59 -0600

    got connection with snowflake working

--- sports_bank/assets/nba_players.py
+++ sports_bank/assets/nba_players.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 from pandas import DataFrame
 import requests 
+import time
 from requests.adapters import HTTPAdapter, Retry
 from . import constants
 from ..resources import snowflake
@@ -13,6 +14,8 @@ from snowflake.connector.pandas_tools import write_pandas
 # Imports for tool
 from dagster import asset, MaterializeResult
 from dagster_snowflake import SnowflakeResource
+import snowflake.connector.errors
+
 
 
 ## TODO
@@ -29,7 +32,7 @@ def call_nba_api(player, max_retries=5, backoff_factor=1):
         return response
 
 @asset
-def player_stats_reg_season_file(database: SnowflakeResource):
+def player_stats_reg_season(database: SnowflakeResource):
     """
         Pandas dataframe for regular season stats for each player.
     """
@@ -37,42 +40,50 @@ def player_stats_reg_season_file(database: SnowflakeResource):
     nba_players = players.get_players()
 
     # Loop through each player and create dataframe
-    player_stats_reg_season = []
+    nba_players_stats_reg_season = []
     for player in nba_players:
-        if player['IsActive'] == 'TRUE':
+        if player['is_active'] == True:
             print(f"Loading data for player: {player['id']}")
             try:
-                career_stats = playercareerstats.PlayerCareerStats(player_id=player['id'], timeout=30)
+                career_stats = playercareerstats.PlayerCareerStats(player_id=player['id'])
                 career_dict = career_stats.get_normalized_dict()
                 reg_season_totals = career_dict['SeasonTotalsRegularSeason']
-                player_stats_reg_season += reg_season_totals
+                nba_players_stats_reg_season += reg_season_totals
             except Exception as e:
                 print(f"Failed API call at {player['id']} with the following exception: {e}")
             finally:
                 # Set time to sleep to prevent rate limits
                 time.sleep(30)
-    player_stats_reg_season_df = pd.DataFrame.from_dict(player_stats_reg_season)
 
-    with database.get_connection() as conn:
-        table_name = "players_stats_reg_season"
-        database = "DB_SPORTSBANK_DEV"
-        schema = "NBA"
-        success, number_chunks, rows_inserted, output = write_pandas(
+    nba_players_stats_reg_season_df = pd.DataFrame.from_dict(nba_players_stats_reg_season) 
+
+    try: 
+        with database.get_connection() as conn:
+            table_name = "player_stats_regular_season"
+            database = "DB_SPORTSBANK_DEV"
+            schema = "NBA"
+            success, number_chunks, rows_inserted, output = write_pandas(
             conn,
-            player_stats_reg_season_df,
+            nba_players_stats_reg_season_df,
             table_name=table_name,
             database=database,
             schema=schema,
             auto_create_table=True,
             overwrite=True,
             quote_identifiers=False,
-        )
+            )
+    except snowflake.connector.errors.ProgrammingError as e:
+        print(f"Failed to write to database because of empty dataframe with the following exception: {e}")
+        raise
+    except Exception as e:
+        print(f"Failed to write to database for with the following exception: {e}")
+        raise
     return MaterializeResult(
         metadata={"rows_inserted": rows_inserted},
     )
 
 @asset
-def players_file(database: SnowflakeResource):
+def player_info(database: SnowflakeResource):
     """
         Pandas dataframe for player information.
     """
